8964e5e4594849cfd1989b0874852792464153a7
ugurtansal <ugur.tansal@gmail.com>
2024-07-06 14:24:02 +0300

    Delet task from list

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-var list=document.getElementById("listOfDay");
-var day=new Date();
-var year=day.getFullYear();
-var month=day.getMonth()+1;//We added 1 to showing true value(Month values between 0 and 11)
-var today=day.getDay();
-var fullDay=today.toString()+"/"+month.toString()+"/"+year.toString();//Combining all
-list.innerHTML="List of today: "+fullDay;//Print top of list
+var info=document.getElementById("infoDay");
+var date=new Date();
+var year=date.getFullYear();    
+var month=date.getMonth()+1;     //We added 1 to showing true value(Month values between 0 and 11)
+var today=date.getDay();
+var fullDay=today.toString()+"/"+month.toString()+"/"+year.toString();  //Combining all date objects
+info.innerHTML="List of today: "+fullDay;   //Print top of list
 
 var form=document.getElementById("addToDo");
 var list=document.getElementById("listTODO");
+var btnDelete=document.getElementById("btnDeleteAll");
 
 eventListeners();
 
 function eventListeners(){
+
+    //Add event
     form.addEventListener("submit",addToList);
+
+    //Delete All Items
+    btnDelete.addEventListener("click",deleteAllItems);
 }
 
-function addToList(e){
+function addToList(){
 
     var input=document.getElementById("txtToDo");
     var liItem=document.createElement("li");
@@ -23,15 +29,24 @@ function addToList(e){
     liItem.className="list-group-item list-group-item-secondary";
     liItem.appendChild(document.createTextNode(input.value));
 
-    var dltBtn=document.createElement("button");
+    var dltBtn=document.createElement("button");        //Creating delete button for every task
     dltBtn.classList="delete-item float-right btn btn-danger";
     dltBtn.innerHTML="Delete";
+    dltBtn.onclick=function(){
+        list.removeChild(this.parentNode);   //Delete an item
+    };
     
 
-    liItem.appendChild(dltBtn);
-    list.appendChild(liItem);//Added to list
+    liItem.appendChild(dltBtn);  //Add button and content to li item
+    list.appendChild(liItem);       //Added to list
 
-    input.value="";//Clear input
+    input.value="";         //Clear input
 
-    e.preventDefault();
+   
+ }
+
+
+ function deleteAllItems(){
+    
+    list.innerHTML="";   //Delete All items
  }
